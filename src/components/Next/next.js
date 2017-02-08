c352c91a3156129b22409f540603b33aea3891a1
trapck <trapck@ukr.net>
2017-02-08 07:51:41 +0200

    header added

--- src/components/Next/next.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Next = () => {
-	return (
-		<div>To be continue ...</div>
-	);
-};
-
-export default Next;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ import BudgetPage from "../components/budget-page/budget-page";
 import HomePage from "../components/home-page/home-page";
 import Next from "../components/next/next";
 
+// temp comment
 export default (
 	<Route path = "/" component = {App}>
 		<IndexRoute component = {HomePage}/>
